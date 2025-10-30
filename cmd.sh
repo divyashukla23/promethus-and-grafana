19762c0b3b21913309e312b7cae36af700878e7b
Divya Shukla <divyashukla20993@gmail.com>
2025-10-30 18:26:57 +0530

    feat: test

--- cmd.sh
+++ cmd.sh
@@ -6,3 +6,13 @@ installation for promethus
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 helm install prometheus prometheus-community/prometheus
+
+
+-------------
+GRAFANA
+helm repo add grafana https://grafana.github.io/helm-charts
+helm repo update
+
+helm install my-grafana grafana/grafana --namespace monitoring --create-namespace
+helm list -n monitoring
+kubectl get pods -n monitoring
